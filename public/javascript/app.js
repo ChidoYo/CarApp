3663e0d965fb96515288efcb867f46e11ca19ee6
Dorje Damdul <luckyyoudude@gmail.com>
2016-11-19 10:51:27 -0700

    changes made in html

---
+++ public/javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  $('#get-weather').on('click', function(){
+    var sendData = $('#city').val() + ',' + $('#country').val();
+    var result = $('#weatherDiv');
+
+    $.ajax ({
+      url: "https://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=jwtgsqpzabba9kgm96ch9pxz";
+      type: 'GET',
+      data: {},
+      dataType: 'JSON',
+      success: function(data){
+        result.html(data);
+      },
+      error: function(data){
+        result.html(data.message);
+      }
+
+    });
+  });
+});
